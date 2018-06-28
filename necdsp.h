a704db77c13ece6425f2ab38ecdc25c111b62791
kps501 <39468366+kps501@users.noreply.github.com>
2018-06-28 11:52:13 -0500

    Add files via upload

---
+++ necdsp.h
@@ -0,0 +1,9 @@
+extern void necdsp_load();
+extern void necdsp_enter();
+
+extern uint8 necdsp_sr_read();
+extern void necdsp_sr_write(uint8 data);
+extern uint8 necdsp_dr_read();
+extern void necdsp_dr_write(uint8 data);
+
+extern bool necdsp_active;
